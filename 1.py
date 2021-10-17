fa71f1c35716b44a88e5a8dcf78935993406e523
Flibora <lucky.maks.tak@gmail.com>
2021-10-17 16:16:30 +0300

    delete

--- 1.py
+++
@@ -1,7 +0,0 @@
-#  dani 2 deistvitelnih 4isla a i b. poluchit ih summy raznost i proizvedenie
-a = 2
-b = 4
-
-print(a + b)
-print(a - b)
-print(a * b)
--- test.py
+++
@@ -1,7 +0,0 @@
-Hello World!
-Knock, knock
-Who is there?
-Git
-Git who?
-Git this joke over with
-
